1b18bb95e9fc2db730c7e1b9df891a8a5f1cf01b
abdallaElnagar-74 <20912020100658@fci.zu.edu.eg>
2024-09-11 16:08:29 +0300

    even-odd

---
+++ even-odd/main.js
@@ -0,0 +1,17 @@
+let x = Number(window.prompt("Enter the Number"));
+
+if(x%2 == 0 ){
+    if(x == 0){
+        console.log("Zero");
+        
+    }
+    else{
+        console.log("Even");
+        
+    }
+    
+}
+else {
+    console.log("odd");
+    
+}
