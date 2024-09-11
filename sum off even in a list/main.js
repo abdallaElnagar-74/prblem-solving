eaf95fd185ed68184621c8aa375f9c4120afad92
abdallaElnagar-74 <20912020100658@fci.zu.edu.eg>
2024-09-11 17:42:24 +0300

    finalRes

--- sum off even in a list/main.js
+++ sum off even in a list/main.js
@@ -1,19 +1,22 @@
-let a = [];
+let a = [-5,-1,1,2];
 let sum = 0;
-function sumOfEvenNums(n) {
+function sumOfPositiveNums(n) {
   for (let i = 0; i < n.length; i++) {
-    if (n[i] % 2 == 0) {
-      if (n[i] >= 0) {
+    if (n[i] >= 0) {
         sum += n[i];
-      } else {
-        continue;
-      }
-    } else {
-      sum = 0;
+    } 
+    else {
       continue;
     }
   }
   return sum;
 }
 
-console.log(sumOfEvenNums(a));
+function sumOfPositiveNumsWithFilter(){
+    let filtered = a.filter(x=>x>0)
+   return filtered.reduce((acc,curr)=>acc + curr , 0)
+     
+}
+console.log(sumOfPositiveNumsWithFilter()); //with filter function
+
+console.log(sumOfPositiveNums(a));  //with for Loop 
