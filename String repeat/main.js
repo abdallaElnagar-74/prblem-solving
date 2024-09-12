da5f8cb9ecda22f6c5b35acaa6848585feba148e
abdallaElnagar-74 <20912020100658@fci.zu.edu.eg>
2024-09-13 01:58:40 +0300

    repeat

--- String repeat/main.js
+++ String repeat/main.js
@@ -1,8 +1,8 @@
-function repeatStr(word,noOfRepeat){
-    let str = ''
-    for (let index = 0 ; index < noOfRepeat ; index++){
-        str+=word
-    }
-    return str
+function repeatStr(word, noOfRepeat) {
+  let str = "";
+  for (let index = 0; index < noOfRepeat; index++) {
+    str += word;
+  }
+  return str;
 }
-console.log(repeatStr("I",10)); //IIIIIIIIII
+console.log(repeatStr("I", 10)); //IIIIIIIIII
