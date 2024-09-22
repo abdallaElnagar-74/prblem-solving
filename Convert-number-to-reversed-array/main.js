b06a691e2b87b67e1cf518984ed78174693f4bc3
abdallaElnagar-74 <abdallasaleh503@gmail.com>
2024-09-22 14:13:10 +0300

    donee

--- Convert-number-to-reversed-array/main.js
+++ Convert-number-to-reversed-array/main.js
@@ -1,13 +1,9 @@
-// a = "5431"
-// wp = a.split('')
-// mp = wp.map(index=> Number(index))
-// rev = mp.toReversed()
-// console.log(rev);
-
-function digitize (n){
-    let a = String(n)
-    return a.split('').map(index=>Number(index)).toReversed()
-
+function digitize(n) {
+  let a = String(n);
+  return a
+    .split("")
+    .map((index) => Number(index))
+    .toReversed();
 }
 
 console.log(digitize(548971));
