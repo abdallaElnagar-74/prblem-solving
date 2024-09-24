e9edd2e601faf56f9a05627aa890de0b5b74aaf8
abdallaElnagar-74 <abdallasaleh503@gmail.com>
2024-09-25 00:23:29 +0300

    sum&count

--- Count-of-positives-sum-of-negatives/main.js
+++ Count-of-positives-sum-of-negatives/main.js
@@ -1,3 +1,4 @@
+// way 1
 function countPositivesSumNegatives(arr) {
   let countPositeves = 0;
   let sumNegatives = 0;
@@ -15,3 +16,18 @@ function countPositivesSumNegatives(arr) {
 }
 
 console.log(countPositivesSumNegatives([-1, -2, -5, -8, -4, 1, 4, 5, 6, 3, 7])); //[6,-20]
+
+// way 2
+
+function countPositivesSumNegatives1(arr) {
+  let positiveCount = arr.filter((n) => n > 0).length;
+  let negativeSum = arr
+    .filter((n) => n < 0)
+    .reduce((prev, curr) => prev + curr, 0);
+
+  return [positiveCount, negativeSum];
+}
+
+console.log(
+  countPositivesSumNegatives1([-1, -2, -5, -8, -4, 1, 4, 5, 6, 3, 7])
+); //[6,-20]
