36bda2472b41f34c61fbbe29987ac5814688e284
abdallaElnagar-74 <abdallasaleh503@gmail.com>
2024-09-25 11:07:57 +0300

    basicOp

--- Basic Mathematical Operations/main.js
+++ Basic Mathematical Operations/main.js
@@ -34,8 +34,8 @@ console.log(basicOp1("*", 5, 5)); //25
 console.log(basicOp1("/", 49, 7)); //7
 
 //way3
-function basicOp2(op, val1, val2){
-    return eval(val1+op+val2)
+function basicOp2(op, val1, val2) {
+  return eval(val1 + op + val2);
 }
 console.log(basicOp2("+", 4, 7)); //11
 console.log(basicOp2("-", 15, 18)); //-3
