4dbaf39751a93e79b70797dcba70e63ccc982472
abdallaElnagar-74 <abdallasaleh503@gmail.com>
2024-09-28 03:14:34 +0300

    noSpace

--- Count By X/main.js
+++ Count By X/main.js
@@ -1,20 +1,20 @@
-// way 1 
+// way 1
 function countByNumber(n, len) {
-    let arr = new Array();
-    for (let i = 1; i <= len; i ++) {
-        arr.push(i*n)
-    }
-    return arr;
+  let arr = new Array();
+  for (let i = 1; i <= len; i++) {
+    arr.push(i * n);
+  }
+  return arr;
 }
 console.log(countByNumber(1, 10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(countByNumber(2,5)); // 
+console.log(countByNumber(2, 5)); //[2, 4, 6, 8, 10]
 
 // way 2
 
-function countByNumber1(n,length){
-
-    return Array.from(Array(length+1).keys()).slice(1).map(i => i*n)
+function countByNumber1(n, length) {
+  return Array.from(Array(length + 1).keys())
+    .slice(1)
+    .map((i) => i * n);
 }
-console.log(countByNumber1(1,10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(countByNumber1(2,5)); //
-
+console.log(countByNumber1(1, 10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(countByNumber1(2, 5)); //[2, 4, 6, 8, 10]
---
+++ Remove String Spaces/main.js
@@ -0,0 +1,4 @@
+function noSpace(word) {
+  return word.split(" ").join("");
+}
+console.log(noSpace("Abdalla Saleh Elnagar"));
